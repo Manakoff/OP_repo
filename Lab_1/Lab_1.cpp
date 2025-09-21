08970e0b6fe8ec4b5a4a166f2ed43c3e3688ff34
Manakov Andrey <manakoooov@gmail.com>
2025-09-21 21:14:43 +0200

    lab1

--- Lab_1/Lab_1.cpp
+++ Lab_1/Lab_1.cpp
@@ -1,14 +1,21 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
 int main()
 {
     setlocale(LC_ALL, "RU");
-    system("chcp 1251");
 
-    cout << "Hello World!" << endl; //Р’С‹РІРѕРґ Hello World!
-    
+
+    double number;
+
+    cout << "Введите неотрицательное число: ";
+    cin >> number;
+
+    cout << number * (-1) << " - противоположное числу " << number << endl;
+    cout << sqrt(number)  << " - значение корня 2-й степени" << endl;
+    cout << pow(number, 1 / 5) << " - значение корня 5-й степени" << endl;
+
     return 0;
 }
-

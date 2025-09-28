4e1049cdb966b5a34666b78ea4ec14a8e649c2aa
Manakov Andrey <manakoooov@gmail.com>
2025-09-28 16:39:34 +0200

    update

--- Lab_1/Lab_1.cpp
+++ Lab_1/Lab_1.cpp
@@ -8,37 +8,27 @@ int main()
 {
     setlocale(LC_ALL, "RU");
 
-
     int number; // т.к int целочисленный тип данных, не взял unsigned, потопму что в условии не сказано
+    cout << "Ввдите целое число: ";
+    cin >> number;
+
     cout << "int - целочисленный тип данных, размер - " << sizeof(number) << " байт" << endl;
     cout << "Минимальное значение: - 2 147 483 648, максимальное: 2 147 483 647" << endl;
     cout << "double - тип данных числа с точкой, размер - " << sizeof(double) << " байт" << endl;
     cout << "Минимальное значение: - 1.7*10^-308, максимальное: 1.7*10^308" << endl;
-    int k{ 1 };
+
+    int newNumber{ (-1) * number };
+    cout << newNumber << " type is int" << endl;
+
+    double sq5Number{ (-1) * pow(abs(number), (0.2)) };
+    cout << sq5Number << " type is double" << endl;
+
+    double sq2Number{ sqrt(number) };
+    cout << sq2Number << " type is double" << endl;
+
     /*
-     Взял цикл для того, чтобы пользователь мог ввести положительное число
+    Использовал double потому что чило может получится как целое так и не целое
     */
-    while (k != 0)
-    {
-        cout << "Введите целое число: ";
-        cin >> number;
-        int protNumber = number * (-1) ;
 
-        cout << protNumber << " - противоположное числу, type - int " << number << endl;
-        if (number >= 0)
-        {
-            double sq_2 = sqrt(number); // т.к может быть и не целое
-            double sq_5 = pow(number, 0.2);
-
-            cout << sq_2 << " - значение корня 2-й степени, type - double" << endl;
-            cout << sq_5 << " - значение корня 5-й степени, type - double" << endl;
-            k = 0;
-        }
-        else
-        {
-            cout << "Число отрицательное" << endl;
-        }
-
-    }
     return 0;
 }

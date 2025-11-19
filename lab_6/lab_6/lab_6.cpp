83f4d21f46c2ca6988f698aa4e8b756092052a3d
Manakov Andrey <manakoooov@gmail.com>
2025-11-19 02:23:31 +0200

    lab 6

---
+++ lab_6/lab_6/lab_6.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+    // Задание № 1
+
+    void *matrix_1 = malloc(sizeof(int) * 4);
+
+    for (int i = 0; i < 4; ++i)
+    {
+        int n;
+        cin >> n;
+        matrix_1[i] = n;
+    }
+
+    // Задание № 2
+    cout << "Введите целое число a и b: ";
+    int a, b;
+    cin >> a >> b;
+
+    int* ptr_a = &a;
+    int* ptr_b = &b;
+
+    int* ptr_obmen = new int;
+
+    *ptr_obmen = *ptr_b;
+
+    *ptr_b = *ptr_a * 2;
+    *ptr_a = *ptr_obmen;
+    delete ptr_obmen;
+    ptr_obmen = 0;
+
+    cout << a  << " " << b;
+    
+
+    return 0;
+}
+

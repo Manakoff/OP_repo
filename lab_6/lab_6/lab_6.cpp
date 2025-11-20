2b981c7e89b182b6a7afc4893ef16505632f15c5
Manakov Andrey <manakoooov@gmail.com>
2025-11-20 19:30:36 +0200

    Для сдачи

--- lab_6/lab_6/lab_6.cpp
+++ lab_6/lab_6/lab_6.cpp
@@ -2,21 +2,131 @@
 #include <stdlib.h>
 using namespace std;
 
+void print_m(int** matrix, int& row, int& colume)
+{
+    for (int i = 0; i < row; ++i)
+    {
+        for (int j = 0; j < colume; ++j)
+        {
+            cout << matrix[i][j] << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+}
+
+void print_m(int* matrix, int& row)
+{
+    for (int i = 0; i < row; ++i)
+    {
+            cout << matrix[i] << " ";
+    }
+    cout << endl;
+
+}
+
+void fun_zupolnenie(int* odn_m, int** matrix, int& row, int& column)
+{
+    
+    int k = 0;
+    
+    for (int i = 0; i < row; ++i)
+    {
+        for (int j = 0; j < column; ++j)
+        {
+            if (matrix[i][j] == 0) {
+                odn_m[k] = i;
+                k += 1;
+                free(matrix[i]);
+                break;
+            }
+        }
+        
+        
+    }
+    
+}
 
 int main()
 {
     setlocale(LC_ALL, "ru");
     // Задание № 1
+    int row = 2;
+    int column = 2;
 
-    void *matrix_1 = malloc(sizeof(int) * 4);
+    //Выделение памяти дляя матрицы 2х2
+    int** matrix = (int**)calloc(row, sizeof(int*));
 
-    for (int i = 0; i < 4; ++i)
+    for (int i = 0; i < row; ++i)
     {
-        int n;
-        cin >> n;
-        matrix_1[i] = n;
+        matrix[i] = (int*)calloc(column, sizeof(int));
     }
 
+    print_m(matrix, row, column);
+
+    //Заполнение матрицы
+    for (int i = 0; i < row; ++i) {
+        for (int j = 0; j < column; ++j)
+        {
+            int num;
+            cin >> num;
+            matrix[i][j] = num;
+        }
+    }
+
+    int A = matrix[0][0]; int B = matrix[0][1]; int C = matrix[1][0]; int D = matrix[1][1];
+    if (A < 0 or B < 0)
+    {
+        cout << "Ошибка попробуйте ещё раз!" << endl;
+    }
+    else
+    {
+
+
+
+
+        row += A;
+        column += B;
+
+        matrix = (int**)calloc(row, sizeof(int*));
+
+        for (int i = 0; i < row; ++i)
+        {
+            matrix[i] = (int*)calloc(column, sizeof(int));
+
+        }
+
+        for (int i = 0; i < row; ++i)
+        {
+            for (int j = 0; j < column; ++j)
+            {
+
+                matrix[i][j] = C * i + D * j;
+            }
+        }
+        print_m(matrix, row, column);
+        int k = 0;
+        for (int i = 0; i < row; ++i)
+        {
+            for (int j = 0; j < column; ++j)
+            {
+
+                if (matrix[i][j] == 0)
+                {
+                    k += 1;
+                    break;
+                }
+            }
+        }
+        int* odn_m = (int*)calloc(k, sizeof(int));
+        fun_zupolnenie(odn_m, matrix, row, column);
+
+        print_m(matrix, row, column);
+        print_m(odn_m, k);
+    }
+    
+
+    
     // Задание № 2
     cout << "Введите целое число a и b: ";
     int a, b;
